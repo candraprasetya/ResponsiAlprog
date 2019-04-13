085fb0fd5ed1e7ac85e91a799288669587e7cf32
Candra Prasetya <candraramadhanp@gmail.com>
2019-04-13 19:13:02 +0700

    Invoice Update

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var transact = new Transaction();
+
+            Process.GetInstance.WriteFile(transact);
+            transact.Sales();
+            transact.PreSales();
+
+            Process.GetInstance.ReadFile(ref transact);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Transaction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Transaction
+    {
+        public string Invoice { get; set; }
+        public string Currdate { get; set; }
+        public Dishes Dish { get; set; }
+
+        public Transaction()
+        {
+            Dish = new Dishes();
+            Invoice = "N1/2019/01";
+            Currdate = Convert.ToDateTime(DateTime.Now).ToString("dd-MM-yy");
+        }
+
+        public void Sales()
+        {
+            var sales = $"Invoice: {Invoice}; Current Date: {Currdate}; ";
+            Console.WriteLine(sales);
+        }
+
+        public void PreSales()
+        {
+            Console.WriteLine(Dish.Info());
+        }
+
+        public void ChangeDue()
+        {
+            if (Dish == null) return;
+            Console.WriteLine(PlusPPn());
+        }
+
+        public double PlusPPn()
+        {
+            var tax = 1.1;
+            tax *= Dish.Price;
+            return tax;
+        }
+    }
+}
